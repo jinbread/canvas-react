5cf75b68720414f256ec31249b80fe046abadd1d
Jinjae Lee <jinjaelee@Administrators-MacBook-Pro-483.local>
2019-02-12 15:58:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      speed: 250
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext("2d")
+    const width = canvas.width;
+    const height = canvas.height;
+    ctx.save();
+    ctx.beginPath();
+    ctx.clearRect(0, 0, width, height);
+    ctx.translate(width / 2, height / 2);
+    ctx.rotate((this.state.speed * Math.PI) / 180);
+    ctx.fillStyle = '#4397AC';
+    ctx.fillRect(-width / 4, -height / 4, width / 2, height / 2);
+    ctx.restore();
+  }
+
+  componentDidUpdate() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext("2d")
+    const width = canvas.width;
+    const height = canvas.height;
+    ctx.save();
+    ctx.beginPath();
+    ctx.clearRect(0, 0, width, height);
+    ctx.translate(width / 2, height / 2);
+    ctx.rotate((this.state.speed * Math.PI) / 180);
+    ctx.fillStyle = '#4397AC';
+    ctx.fillRect(-width / 4, -height / 4, width / 2, height / 2);
+    ctx.restore();
+  }
+
+  handleChange(e) {
+    this.setState({
+      speed: e.target.value
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div>
+            <canvas 
+              ref="canvas" 
+              width={this.state.speed} 
+              height={this.state.speed} 
+              style={{border: '2px dashed white'}}
+            />
+            <p>Value: {this.state.speed}</p>
+            <input 
+              type="range"
+              min={0}
+              max={500}
+              value={this.state.rotationSpeed}
+              onChange={this.handleChange}
+              name="speed"
+            />
+          </div>
         </header>
       </div>
     );
